45db58e99b85560ef6081749e8cd778afad9a530
mmarszik <mmarszik@gmail.com>
2019-12-25 00:36:28 +0100

    .

--- rnd_add_rot.h
+++ rnd_add_rot.h
@@ -58,49 +58,45 @@
 template<class TRND, CMRND_UINT SIZE1, CMRND_UINT SIZE2>
 class RndAddRot : public RndBase {
 private:
-    using TBUFF1 = MxArray<TYPE_RESULT, SIZE1>;
-    using TBUFF2 = MxArray<TYPE_RESULT, SIZE2>;
-
-private:
     TRND       rnd;
-    TBUFF1     buff1;
-    TBUFF2     buff2;
-    TMRND_UINT i1, i2;
+    TMRND_UINT *const buff;
+    TMRND_UINT *i1,*i2;
+    TMRND_UINT *const end1, *const end2;
 
 private:
     void reset() {
-        for( TMRND_UINT i=0 ; i<SIZE1 ; i++ )
-            buff1[i] = rnd();
-        for( TMRND_UINT i=0 ; i<SIZE2 ; i++ )
-            buff2[i] = rnd();
+        for( TMRND_UINT i=0 ; i<SIZE1+SIZE2 ; i++ ) {
+            buff[i] = rnd();
+        }
     }
-
-    static TYPE_RESULT rot( TYPE_RESULT &v ) {
-        return v = (v << 1) | ( v >> ( MLimits<TYPE_RESULT>::digits() - 1 ) );
+    static TYPE_RESULT rot( TYPE_RESULT *v ) {
+        return *v = (*v << 1) | ( *v >> ( MLimits<TYPE_RESULT>::digits() - 1 ) );
     }
-
 public:
-    RndAddRot( const RndBase::TYPE_RESULT __sd ) {
+    RndAddRot( const RndBase::TYPE_RESULT __sd ) : buff(new TMRND_UINT[SIZE1+SIZE2]), end1(buff+SIZE1),end2(buff+SIZE1+SIZE2) {
         seed( __sd );
     }
-
+    ~RndAddRot() {
+        delete[] buff;
+    }
     void seed( const RndBase::TYPE_RESULT __sd ) {
         rnd.seed( __sd );
-        i1 = SIZE1;
-        i2 = SIZE2;
+        i1 = end1;
+        i2 = end2;
     }
-
     TMRND_UINT operator()() {
-        if( i1 == SIZE1 && i2 == SIZE2 ) {
-            reset();
+        if( i1 == end1 ) {
+            if( i2 == end2 ) {
+                reset();
+            }
+            i1 = buff;
         }
-        if( i1 == SIZE1 ) i1 = 0;
-        if( i2 == SIZE2 ) i2 = 0;
-        return rot(buff1[i1++]) + rot(buff2[i2++]);
+        if( i2 == end2 ) {
+            i2 = end1;
+        }
+        return rot(i1++) + rot(i2++);
     }
-
 };
-
 
 
 using RndAddRot0a  = RndAddRot<std::mt19937_64 ,   3061,  2711 >;
@@ -127,3 +123,258 @@ using RndAddRot6c  = RndAddRot<RndFib3         ,   3011,  1973 >;
 using RndAddRot7a  = RndAddRot<RndFib3a        ,   3061,  2711 >;
 using RndAddRot7b  = RndAddRot<RndFib3a        ,   3019,  2957 >;
 using RndAddRot7c  = RndAddRot<RndFib3a        ,   3011,  1973 >;
+
+
+//template<class TRND, CMRND_UINT SIZE1, CMRND_UINT SIZE2>
+//class RndAddRot : public RndBase {
+//private:
+//    using TBUFF1 = MxArray<TYPE_RESULT, SIZE1>;
+//    using TBUFF2 = MxArray<TYPE_RESULT, SIZE2>;
+
+//private:
+//    TRND       rnd;
+//    TBUFF1     buff1;
+//    TBUFF2     buff2;
+//    TMRND_UINT i1, i2;
+
+//private:
+//    void reset() {
+//        for( TMRND_UINT i=0 ; i<SIZE1 ; i++ )
+//            buff1[i] = rnd();
+//        for( TMRND_UINT i=0 ; i<SIZE2 ; i++ )
+//            buff2[i] = rnd();
+//    }
+
+//    static TYPE_RESULT rot( TYPE_RESULT &v ) {
+//        return v = (v << 1) | ( v >> ( MLimits<TYPE_RESULT>::digits() - 1 ) );
+//    }
+
+//public:
+//    RndAddRot( const RndBase::TYPE_RESULT __sd ) {
+//        seed( __sd );
+//    }
+
+//    void seed( const RndBase::TYPE_RESULT __sd ) {
+//        rnd.seed( __sd );
+//        i1 = SIZE1;
+//        i2 = SIZE2;
+//    }
+
+//    TMRND_UINT operator()() {
+//        if( i1 == SIZE1 && i2 == SIZE2 ) {
+//            reset();
+//        }
+//        if( i1 == SIZE1 ) i1 = 0;
+//        if( i2 == SIZE2 ) i2 = 0;
+//        return rot(buff1[i1++]) + rot(buff2[i2++]);
+//    }
+
+//};
+
+
+//template<class TRND, CMRND_UINT SIZE1, CMRND_UINT SIZE2>
+//class RndAddRot : public RndBase {
+//private:
+//    using TBUFF1 = MxArray<TYPE_RESULT, SIZE1>;
+//    using TBUFF2 = MxArray<TYPE_RESULT, SIZE2>;
+
+//private:
+//    TRND       rnd;
+//    TBUFF1     buff1;
+//    TBUFF2     buff2;
+//    TMRND_UINT i1, i2;
+
+//private:
+//    void reset() {
+//        for( TMRND_UINT i=0 ; i<SIZE1 ; i++ )
+//            buff1[i] = rnd();
+//        for( TMRND_UINT i=0 ; i<SIZE2 ; i++ )
+//            buff2[i] = rnd();
+//    }
+
+//    static TYPE_RESULT rot( TYPE_RESULT &v ) {
+//        return v = (v << 1) | ( v >> ( MLimits<TYPE_RESULT>::digits() - 1 ) );
+//    }
+
+//public:
+//    RndAddRot( const RndBase::TYPE_RESULT __sd ) {
+//        seed( __sd );
+//    }
+
+//    void seed( const RndBase::TYPE_RESULT __sd ) {
+//        rnd.seed( __sd );
+//        i1 = SIZE1;
+//        i2 = SIZE2;
+//    }
+
+//    TMRND_UINT operator()() {
+//        if( i1 == SIZE1 ) {
+//            if( i2 == SIZE2 ) {
+//                reset();
+//            }
+//            i1 = 0;
+//        }
+//        if( i2 == SIZE2 ) i2 = 0;
+//        return rot(buff1[i1++]) + rot(buff2[i2++]);
+//    }
+//};
+
+
+//template<class TRND, CMRND_UINT SIZE1, CMRND_UINT SIZE2>
+//class RndAddRot : public RndBase {
+//private:
+//    using TBUFF1 = MxArray<TYPE_RESULT, SIZE1>;
+//    using TBUFF2 = MxArray<TYPE_RESULT, SIZE2>;
+
+//private:
+//    TRND       rnd;
+//    TBUFF1     buff1;
+//    TBUFF2     buff2;
+//    TMRND_UINT *i1;
+//    TMRND_UINT *i2;
+//    TMRND_UINT *const end1;
+//    TMRND_UINT *const end2;
+
+//private:
+//    void reset() {
+//        for( TMRND_UINT i=0 ; i<SIZE1 ; i++ )
+//            buff1[i] = rnd();
+//        for( TMRND_UINT i=0 ; i<SIZE2 ; i++ )
+//            buff2[i] = rnd();
+//    }
+
+//    static TYPE_RESULT rot( TYPE_RESULT &v ) {
+//        return v = (v << 1) | ( v >> ( MLimits<TYPE_RESULT>::digits() - 1 ) );
+//    }
+
+//public:
+//    RndAddRot( const RndBase::TYPE_RESULT __sd ) : end1(&buff1[0]+SIZE1),end2(&buff2[0]+SIZE2) {
+//        seed( __sd );
+//    }
+
+//    void seed( const RndBase::TYPE_RESULT __sd ) {
+//        rnd.seed( __sd );
+//        i1 = end1;
+//        i2 = end2;
+//    }
+
+//    TMRND_UINT operator()() {
+//        if( i1 == end1 ) {
+//            if( i2 == end2 ) {
+//                reset();
+//            }
+//            i1 = &buff1[0];
+//        }
+//        if( i2 == end2 ) i2 = &buff2[0];
+//        return rot(*i1++) + rot(*i2++);
+//    }
+
+//};
+
+//template<class TRND, CMRND_UINT SIZE1, CMRND_UINT SIZE2>
+//class RndAddRot : public RndBase {
+//private:
+//    using TBUFF1 = MxArray<TYPE_RESULT, SIZE1>;
+//    using TBUFF2 = MxArray<TYPE_RESULT, SIZE2>;
+
+//private:
+//    TRND       rnd;
+//    TBUFF1     buff1;
+//    TBUFF2     buff2;
+//    TMRND_UINT *i1;
+//    TMRND_UINT *i2;
+//    TMRND_UINT *const end1;
+//    TMRND_UINT *const end2;
+
+
+//private:
+//    void reset() {
+//        for( TMRND_UINT i=0 ; i<SIZE1 ; i++ )
+//            buff1[i] = rnd();
+//        for( TMRND_UINT i=0 ; i<SIZE2 ; i++ )
+//            buff2[i] = rnd();
+//    }
+
+//    static TYPE_RESULT rot( TYPE_RESULT *v ) {
+//        return *v = (*v << 1) | ( *v >> ( MLimits<TYPE_RESULT>::digits() - 1 ) );
+//    }
+
+//public:
+//    RndAddRot( const RndBase::TYPE_RESULT __sd ) : end1(&buff1[0]+SIZE1),end2(&buff2[0]+SIZE2) {
+//        seed( __sd );
+//    }
+
+//    void seed( const RndBase::TYPE_RESULT __sd ) {
+//        rnd.seed( __sd );
+//        i1 = end1;
+//        i2 = end2;
+//    }
+
+//    TMRND_UINT operator()() {
+//        if( i1 == end1 ) {
+//            if( i2 == end2 ) {
+//                reset();
+//            }
+//            i1 = &buff1[0];
+//        }
+//        if( i2 == end2 ) i2 = &buff2[0];
+//        return rot(i1++) + rot(i2++);
+//    }
+
+//};
+
+
+//template<class TRND, CMRND_UINT SIZE1, CMRND_UINT SIZE2>
+//class RndAddRot : public RndBase {
+//private:
+//    TRND       rnd;
+//    TMRND_UINT *const buff1;
+//    TMRND_UINT *const buff2;
+//    TMRND_UINT *i1;
+//    TMRND_UINT *i2;
+//    TMRND_UINT *const end1;
+//    TMRND_UINT *const end2;
+
+
+//private:
+//    void reset() {
+//        for( TMRND_UINT i=0 ; i<SIZE1 ; i++ )
+//            buff1[i] = rnd();
+//        for( TMRND_UINT i=0 ; i<SIZE2 ; i++ )
+//            buff2[i] = rnd();
+//    }
+
+//    static TYPE_RESULT rot( TYPE_RESULT *v ) {
+//        return *v = (*v << 1) | ( *v >> ( MLimits<TYPE_RESULT>::digits() - 1 ) );
+//    }
+
+//public:
+//    RndAddRot( const RndBase::TYPE_RESULT __sd ) : buff1(new TMRND_UINT[SIZE1]), buff2(new TMRND_UINT[SIZE2]), end1(&buff1[0]+SIZE1),end2(&buff2[0]+SIZE2) {
+//        seed( __sd );
+//    }
+
+//    ~RndAddRot() {
+//        delete[] buff1;
+//        delete[] buff2;
+//    }
+
+//    void seed( const RndBase::TYPE_RESULT __sd ) {
+//        rnd.seed( __sd );
+//        i1 = end1;
+//        i2 = end2;
+//    }
+
+//    TMRND_UINT operator()() {
+//        if( i1 == end1 ) {
+//            if( i2 == end2 ) {
+//                reset();
+//            }
+//            i1 = &buff1[0];
+//        }
+//        if( i2 == end2 ) i2 = &buff2[0];
+//        return rot(i1++) + rot(i2++);
+//    }
+
+//};
+
+
